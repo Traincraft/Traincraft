7238df57257e837493b8591715fe8b4d3b9ad4de
canitzp <canitzp@googlemail.com>
2016-05-13 07:34:55 +0200

    Some Crafter render ans Slot works

--- src/main/java/si/meansoft/traincraft/container/ContainerCrafterSteam.java
+++ src/main/java/si/meansoft/traincraft/container/ContainerCrafterSteam.java
@@ -2,15 +2,40 @@ package si.meansoft.traincraft.container;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 import net.minecraft.tileentity.TileEntity;
+import si.meansoft.traincraft.tileEntities.TileEntityCrafterSteam;
 
 /**
  * @author canitzp
  */
 public class ContainerCrafterSteam extends Container {
 
-    public ContainerCrafterSteam(EntityPlayer player, TileEntity tileEntity){
+    private final TileEntityCrafterSteam crafter;
+    private final EntityPlayer player;
 
+    public ContainerCrafterSteam(EntityPlayer player, TileEntity tileEntity){
+        this.player = player;
+        this.crafter = (TileEntityCrafterSteam) tileEntity;
+        for(int j = 0; j < 3; ++j) {
+            for(int k = 0; k < 9; ++k) {
+                this.addSlotToContainer(new Slot(player.inventory, k + j * 9 + 9, 8 + k * 18, 172 + j * 18));
+            }
+        }
+        for(int j = 0; j < 9; ++j) {
+            this.addSlotToContainer(new Slot(player.inventory, j, 8 + j * 18, 230));
+        }
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 2 ; j++){
+                addSlotToContainer(new Slot(this.crafter, i + j * 4, 8 + i * 18, 126 + j * 18));
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 2 ; j++){
+                addSlotToContainer(new Slot(this.crafter, i + j * 4 + 8, 92 + i * 18, 126 + j * 18));
+            }
+        }
     }
 
     @Override
