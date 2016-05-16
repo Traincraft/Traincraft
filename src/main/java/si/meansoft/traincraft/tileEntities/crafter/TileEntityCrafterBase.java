26520e148da8cf790e7be5996200b48c0b22ca07
Ellpeck <megamaximal@gmail.com>
2016-05-16 03:14:48 +0200

    subsequently also fix this

--- src/main/java/si/meansoft/traincraft/tileEntities/crafter/TileEntityCrafterBase.java
+++ src/main/java/si/meansoft/traincraft/tileEntities/crafter/TileEntityCrafterBase.java
@@ -2,6 +2,7 @@ package si.meansoft.traincraft.tileEntities.crafter;
 
 import net.minecraft.util.ITickable;
 import si.meansoft.traincraft.api.recipes.CrafterRecipes;
+import si.meansoft.traincraft.api.recipes.CrafterRecipes.Recipe;
 import si.meansoft.traincraft.blocks.BlockCrafter;
 import si.meansoft.traincraft.tileEntities.TileEntityInventory;
 
@@ -16,20 +17,18 @@ public abstract class TileEntityCrafterBase extends TileEntityInventory implemen
 
     @Override
     public void update(){
+        Recipe recipe = new Recipe(getStackInSlot(16), getStackInSlot(17), getStackInSlot(18), getStackInSlot(19), getStackInSlot(20), getStackInSlot(21), getStackInSlot(22), getStackInSlot(23), getStackInSlot(24), getStackInSlot(25));
         switch(this.getTier()){
             case STEAM:{
-                CrafterRecipes.RecipeHandlerSteam recipeHandler = new CrafterRecipes.RecipeHandlerSteam(getStackInSlot(16), getStackInSlot(17), getStackInSlot(18), getStackInSlot(19), getStackInSlot(20), getStackInSlot(21), getStackInSlot(22), getStackInSlot(23), getStackInSlot(24), getStackInSlot(25));
-                CrafterRecipes.craftSteam(this, recipeHandler);
+                CrafterRecipes.craftSteam(this, recipe);
                 break;
             }
             case DIESEL:{
-                CrafterRecipes.Recipe recipeHandler = new CrafterRecipes.Recipe(getStackInSlot(16), getStackInSlot(17), getStackInSlot(18), getStackInSlot(19), getStackInSlot(20), getStackInSlot(21), getStackInSlot(22), getStackInSlot(23), getStackInSlot(24), getStackInSlot(25));
-                CrafterRecipes.craftDiesel(this, recipeHandler);
+                CrafterRecipes.craftDiesel(this, recipe);
                 break;
             }
             case ELECTRO:{
-                CrafterRecipes.RecipeHandlerElectro recipeHandler = new CrafterRecipes.RecipeHandlerElectro(getStackInSlot(16), getStackInSlot(17), getStackInSlot(18), getStackInSlot(19), getStackInSlot(20), getStackInSlot(21), getStackInSlot(22), getStackInSlot(23), getStackInSlot(24), getStackInSlot(25));
-                CrafterRecipes.craftElectro(this, recipeHandler);
+                CrafterRecipes.craftElectro(this, recipe);
                 break;
             }
         }
