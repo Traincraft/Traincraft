bd5a24682c4ee77ee148df2fa152509a73d10f21
Vebert <pep.polet@gmail.com>
2016-07-26 17:19:34 +0200

    Fix slope bug when walking on and TrackBuilder Crash
    - Fix player stuck in slope bug by allowing tile entity synchronization
    for TileTCRailGag.
    - Fix TrackBuilder Crach by checking if railcraft INGOT_STEEL != null
    before using it.

--- src/main/java/train/common/tile/TileTCRailGag.java
+++ src/main/java/train/common/tile/TileTCRailGag.java
@@ -3,6 +3,7 @@ package train.common.tile;
 import java.util.Random;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
@@ -49,5 +50,11 @@ public class TileTCRailGag extends TileEntity {
 		this.writeToNBT(nbt);
 
 		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, nbt);
+	}
+	
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt){
+		this.readFromNBT(pkt.func_148857_g());
+		super.onDataPacket(net, pkt);
 	}
 }
