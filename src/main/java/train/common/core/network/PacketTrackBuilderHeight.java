d8f6eb53d6686834749ab29e8279c8378a2d12da
NitroxydeX <hello@nitroxydex.net>
2016-08-17 18:36:20 +0200

    Sending Server-Message over Client-Side? removed unsued Zeppelin import on Track-Builder Network

--- src/main/java/train/common/core/network/PacketTrackBuilderHeight.java
+++ src/main/java/train/common/core/network/PacketTrackBuilderHeight.java
@@ -6,7 +6,6 @@ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.Entity;
 import train.common.entity.rollingStock.EntityTracksBuilder;
-import train.common.entity.zeppelin.AbstractZeppelin;
 
 public class PacketTrackBuilderHeight implements IMessage {
 
