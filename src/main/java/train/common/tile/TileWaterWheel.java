827027a298d82d432cda4d621cff702fe4540e9c
NitroxydeX <hello@nitroxydex.net>
2016-09-02 00:40:24 +0200

    Go home you're drunk waterwheel!

--- src/main/java/train/common/tile/TileWaterWheel.java
+++ src/main/java/train/common/tile/TileWaterWheel.java
@@ -1,5 +1,6 @@
 package train.common.tile;
 
+import cofh.api.energy.IEnergyProvider;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
@@ -10,22 +11,14 @@ import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 import train.common.core.util.Energy;
 
-public class TileWaterWheel extends Energy {
-	private int facingMeta;
-	private int waterDirection;
+public class TileWaterWheel extends Energy implements IEnergyProvider {
+	
+	public int facingMeta;
 
 	public TileWaterWheel() {
 		super(0, "WaterWheel", 80, 80);
 		super.setSides(new ForgeDirection[]{ForgeDirection.EAST, ForgeDirection.WEST, ForgeDirection.NORTH, ForgeDirection.SOUTH});
 		facingMeta = this.blockMetadata;
-	}
-
-	public int getFacing() {
-		return facingMeta;
-	}
-
-	public void setFacing(int facing) {
-		this.facingMeta = facing;
 	}
 
 	@Override
@@ -67,68 +60,34 @@ public class TileWaterWheel extends Energy {
 			Block blockBottom = worldObj.getBlock(xCoord, yCoord-1, zCoord);
 
 
-			if(blockXP instanceof BlockLiquid && blockXP.getMaterial().isLiquid() && worldObj.getBlockMetadata(xCoord+1, yCoord, zCoord)!= 0 && this.getBlockMetadata()!=2 && blockXP.getMaterial() != Material.lava){
-					worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 2,2);
-
-			}else if(blockXN instanceof BlockLiquid && blockXN.getMaterial().isLiquid() && worldObj.getBlockMetadata(xCoord-1, yCoord, zCoord)!= 0 && this.getBlockMetadata()!=0 && blockXN.getMaterial() != Material.lava){
-					worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 0,2);
-
-			}else if(blockZN instanceof BlockLiquid && blockZN.getMaterial().isLiquid() && worldObj.getBlockMetadata(xCoord, yCoord, zCoord-1)!= 0 && this.getBlockMetadata()!=1 && blockZN.getMaterial() != Material.lava){
-					worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 1,2);
-
-			}else if(blockZP instanceof BlockLiquid && blockZP.getMaterial().isLiquid() && worldObj.getBlockMetadata(xCoord, yCoord, zCoord+1)!= 0 && this.getBlockMetadata()!=3 && blockZP.getMaterial() != Material.lava){
-					worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 3,2);
-
+			if (blockXP instanceof BlockLiquid && blockXP.getMaterial().isLiquid()
+					&& worldObj.getBlockMetadata(xCoord + 1, yCoord, zCoord) != 0
+					&& blockXP.getMaterial() != Material.lava) {
+				this.energy.receiveEnergy(5, false);
+				worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 2, 2);
+			} else if (blockXN instanceof BlockLiquid && blockXN.getMaterial().isLiquid()
+					&& worldObj.getBlockMetadata(xCoord - 1, yCoord, zCoord) != 0
+					&& blockXN.getMaterial() != Material.lava) {
+				this.energy.receiveEnergy(5, false);
+				worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 0, 2);
+			} else if (blockZN instanceof BlockLiquid && blockZN.getMaterial().isLiquid()
+					&& worldObj.getBlockMetadata(xCoord, yCoord, zCoord - 1) != 0
+					&& blockZN.getMaterial() != Material.lava) {
+				this.energy.receiveEnergy(5, false);
+				worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 1, 2);
+			} else if (blockZP instanceof BlockLiquid && blockZP.getMaterial().isLiquid()
+					&& worldObj.getBlockMetadata(xCoord, yCoord, zCoord + 1) != 0
+					&& blockZP.getMaterial() != Material.lava) {
+				this.energy.receiveEnergy(5, false);
+				worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 3, 2);
 			}else if(blockTop instanceof BlockLiquid && blockTop.getMaterial().isLiquid()&&worldObj.getBlockMetadata(xCoord, yCoord+1, zCoord)!= 0 && blockTop.getMaterial() != Material.lava){
-					setWaterDir((int) BlockLiquid.getFlowDirection(this.worldObj,this.xCoord, this.yCoord+1, this.zCoord, blockTop.getMaterial()));
-
+				this.energy.receiveEnergy(5, false);
 			}else if(blockBottom instanceof BlockLiquid && blockBottom.getMaterial().isLiquid() &&worldObj.getBlockMetadata(xCoord, yCoord-1, zCoord)!= 0 && blockBottom.getMaterial() != Material.lava){
-					double dir = BlockLiquid.getFlowDirection(this.worldObj,this.xCoord, this.yCoord-1, this.zCoord,blockBottom.getMaterial());
-
-					if((int)dir == -3||(int)dir==-2){
-						setWaterDir(0);
-					}else if((int)dir == 0){
-						setWaterDir(-3);
-					}else if((int)dir==-1){
-						setWaterDir(1);
-					}else if((int)dir==1){
-						setWaterDir(-1);
-					}
+				this.energy.receiveEnergy(5, false);
 			} else {
-				setWaterDir(-1001);
+				setWaterDir(-1);
 			}
 
-			switch (this.getWaterDir()) {
-
-				case -3:
-				case -2:
-					if (this.getBlockMetadata() != 1) {
-						this.worldObj.setBlockMetadataWithNotify(this.xCoord, this.yCoord, this.zCoord, 1, 2);
-					}
-					this.energy.receiveEnergy(5, false);
-					break;
-
-				case -1:
-					if (this.getBlockMetadata() != 2) {
-						this.worldObj.setBlockMetadataWithNotify(this.xCoord, this.yCoord, this.zCoord, 2, 2);
-					}
-					this.energy.receiveEnergy(5, false);
-					break;
-
-				case 0:
-					if (this.getBlockMetadata() != 3) {
-						this.worldObj.setBlockMetadataWithNotify(this.xCoord, this.yCoord, this.zCoord, 3, 2);
-					}
-					this.energy.receiveEnergy(5, false);
-					break;
-
-				case 1:
-					if (this.getBlockMetadata() != 0) {
-						this.worldObj.setBlockMetadataWithNotify(this.xCoord, this.yCoord, this.zCoord, 0, 2);
-					}
-					this.energy.receiveEnergy(5, false);
-					break;
-			}
 			if (this.energy.getEnergyStored() >0) {
 				pushEnergy(worldObj, this.xCoord, this.yCoord, this.zCoord, this.energy);
 			}
@@ -138,12 +97,14 @@ public class TileWaterWheel extends Energy {
 		}
 
 	}
-
-	private void setWaterDir(int i){
-		waterDirection=i;
+	
+	private void setWaterDir(int i) {
+		facingMeta = i;
+		
 	}
-	public int getWaterDir(){
-		return waterDirection;
+	
+	public int getWaterDir() {
+		return facingMeta;
 	}
 
 	@Override
