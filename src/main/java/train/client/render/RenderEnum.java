fd0666c1f9729d354c4dc3a8470d5ac895c0c480
NitroxydeX <hello@nitroxydex.net>
2017-09-10 23:44:18 +0200

    Fixed GS4 Tender wrong render direction - removed old EMD

--- src/main/java/train/client/render/RenderEnum.java
+++ src/main/java/train/client/render/RenderEnum.java
@@ -102,7 +102,8 @@ public enum RenderEnum {
 	tenderC62Class(EntityTenderC62Class.class, new ModelTenderC62Class(), "c62_class_tender_", true,
 			new float[] { 0.0F, -0.42F, 0.0F }, null, null, "", 0, null, "", null, 0, false),
 	tenderAdler(EntityTenderAdler.class, new ModelTenderAdler(), "tender_adler", false, new float[] { 0.0F, 1.05F, 0.0F }, new float[] { 180F, -90F, 0F }, null, "", 0, null, "", null, 0, false),
-	tenderGS4(EntityTenderGS4.class, new ModelGS4Tender(), "GS4_tender", false, new float[] { 0.0F, 0.18F, 0.0F }, new float[] { 0F, 180F, 180F }, new float[]{0.9f,1f,0.9f}, "", 0, null, "", null, 0, false),
+	tenderGS4(EntityTenderGS4.class, new ModelGS4Tender(), "GS4_tender", false, new float[] { 0.0F, 0.18F, 0.0F },
+			new float[] { 0F, 0F, 180F }, new float[] { 0.9f, 1f, 0.9f }, "", 0, null, "", null, 0, false),
 
 	/** Diesel */
 	locoKOF(EntityLocoDieselKof_DB.class, new ModelKof(), "locoKof_DB_", true, new float[] { -1.0F, -0.44F, 0.0F }, null, null, "largesmoke", 2, new ArrayList<double[]>() {
