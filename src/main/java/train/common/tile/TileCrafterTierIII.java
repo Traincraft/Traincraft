612db0744eee527b49188609bb1bfded4f11776c
NitroxydeX <hello@nitroxydex.net>
2017-09-11 08:11:38 +0200

    Fixed a bug in Crafting Bench Tier 3
    result list was not rendered

--- src/main/java/train/common/tile/TileCrafterTierIII.java
+++ src/main/java/train/common/tile/TileCrafterTierIII.java
@@ -1,5 +1,9 @@
 package train.common.tile;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
@@ -15,10 +19,6 @@ import train.common.core.interfaces.ITier;
 import train.common.core.managers.TierRecipe;
 import train.common.core.managers.TierRecipeManager;
 import train.common.library.Info;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class TileCrafterTierIII extends TileEntity implements IInventory, ITier {
 	private Random rand;
@@ -167,11 +167,11 @@ public class TileCrafterTierIII extends TileEntity implements IInventory, ITier 
 		for (int j = 0; j < recipes.size(); j++) {
 			ItemStack stack = recipes.get(j).hasComponents(crafterInventory);
 			if (stack != null) {
-				resultList.add(stack);
-				if (crafterInventory.length< count+10) {
+				if ((count + 10) < crafterInventory.length) {
+					resultList.add(stack);
 					crafterInventory[count + 10] = new ItemStack(stack.getItem(), 1, 0);
-					count++;
 				}
+				count++;
 			}
 		}
 
