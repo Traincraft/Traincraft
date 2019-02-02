d310a02d56909e63e5a1cea31de0e941513a2b2c
Jonathan <jakedude435@live.com>
2019-02-02 14:22:41 -0600

    missed a thing
    - Linking distance fix that got missed last push.

--- src/main/java/train/common/entity/rollingStock/EntityTender_C41.java
+++ src/main/java/train/common/entity/rollingStock/EntityTender_C41.java
@@ -111,7 +111,7 @@ public class EntityTender_C41 extends Tender implements IInventory {
 
 	@Override
 	public float getOptimalDistance(EntityMinecart cart) {
-		return 1.8F;
+		return 1.75F;
 	}
 
 	@Override
