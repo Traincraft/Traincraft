303a9bc3e1eb72c0e6bd2e079bf1d99468fc0d15
Jonathan <jakedude435@live.com>
2020-12-18 12:50:07 -0600

    modID update and BP4 update
    - ModID changed from tc to traincraft, resources moved accordingly.
    - Updated BP4 model by bidahochi

--- src/main/java/train/library/Info.java
+++ src/main/java/train/library/Info.java
@@ -10,7 +10,7 @@ package train.library;
 public class Info {
 
 	/* Mod relevant information */
-	public static final String modID = "tc";
+	public static final String modID = "traincraft";
 	public static final String modName = "Traincraft";
 	public static final String		modVersion				= "@VERSION1@";
 	public static final String channel = "Traincraft";
@@ -20,7 +20,7 @@ public class Info {
 	/* Localization keys for versioning */
 
 	/* All the resources for the mod */
-	public static final String resourceLocation = "tc";
+	public static final String resourceLocation = "traincraft";
 	public static final String guiPrefix = "textures/gui/";
 	public static final String bookPrefix = "textures/gui/book/";
 	public static final String trainsPrefix = "textures/trains/";
