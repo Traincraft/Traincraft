c93a6206a1f939d64460e14a5b6f894e68199ea7
zetti68 <zetti68@online.de>
2022-03-27 15:10:28 +0200

    err: nothing added to entries

--- src/main/java/train/common/core/handlers/RecipeHandler.java
+++ src/main/java/train/common/core/handlers/RecipeHandler.java
@@ -29,19 +29,21 @@ import train.common.recipes.RecipesArmorDyes;
 
 public class RecipeHandler {
 
-	private static ArrayList<ItemStack> multiNameOreDict(String ... names){
+	private static ArrayList<ItemStack> multiNameOreDict0(String ... names){
 		ArrayList<ItemStack> entries = new ArrayList<ItemStack>();
+		Set<Item> addedItems = new HashSet<>();
 		for (String name : names){
-			List<ItemStack> dir =OreDictionary.getOres(name);
-			for (ItemStack s : dir){
-				if(!dir.contains(s)){
-					dir.add(s);
+			for(ItemStack itemStack : OreDictionary.getOres(name)) {
+				Item item = itemStack.getItem();
+				if(!addedItems.contains(item)) {
+					addedItems.add(item);
+					entries.add(itemStack);
 				}
 			}
 		}
 		return entries;
 	}
-
+	
 	public static void initBlockRecipes() {
 		ArrayList<ItemStack> iron = OreDictionary.getOres("ingotIron");
 
