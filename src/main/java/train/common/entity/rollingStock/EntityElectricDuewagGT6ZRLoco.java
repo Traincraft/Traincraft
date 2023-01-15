39576904dc9123af755807cc18c39c40587621ba
CBwagen <yugocris3@gmail.com>
2023-01-15 17:50:31 +0100

    even more linking distance

--- src/main/java/train/common/entity/rollingStock/EntityElectricDuewagGT6ZRLoco.java
+++ src/main/java/train/common/entity/rollingStock/EntityElectricDuewagGT6ZRLoco.java
@@ -138,7 +138,7 @@ public class EntityElectricDuewagGT6ZRLoco extends ElectricTrain {
     }
     @Override
     public float getOptimalDistance(EntityMinecart cart) {
-        return 0.8F;
+        return 0.65F;
     }
 
     @Override
