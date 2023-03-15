9a1490aac33ce4ab642e99840435119b6096a013
TheDoctor1138 <slimestarr1138@gmail.com>
2023-03-15 20:26:03 +0100

    the actual fix

--- src/main/java/train/common/entity/rollingStock/EntityLocoElectricClass401.java
+++ src/main/java/train/common/entity/rollingStock/EntityLocoElectricClass401.java
@@ -35,7 +35,7 @@ public class EntityLocoElectricClass401 extends ElectricTrain {
     public void updateRiderPosition() {
         if (riddenByEntity == null) {return;}
         double pitchRads = this.anglePitchClient * Math.PI / 180.0D;
-        double distance = 3.9;
+        double distance = 4.0;
         double yOffset = -0.05;
         float rotationCos1 = (float) Math.cos(Math.toRadians(this.renderYaw + 90));
         float rotationSin1 = (float) Math.sin(Math.toRadians((this.renderYaw + 90)));
