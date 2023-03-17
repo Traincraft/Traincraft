a5611e7abd6e10bf13a06591c0e8752cee5875d4
CBwagen <yugocris3@gmail.com>
2023-03-18 00:20:22 +0100

    Second bogie line for GT6
    Added DeLijn of code for second bogie

--- src/main/java/train/common/entity/rollingStock/EntityPassengerDuewagGT6ZRTail.java
+++ src/main/java/train/common/entity/rollingStock/EntityPassengerDuewagGT6ZRTail.java
@@ -6,8 +6,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import train.common.api.EntityRollingStock;
 import train.common.api.IPassenger;
+import train.common.api.ISecondBogie;
 
-public class EntityPassengerDuewagGT6ZRTail extends EntityRollingStock implements IPassenger {
+public class EntityPassengerDuewagGT6ZRTail extends EntityRollingStock implements IPassenger, ISecondBogie {
     //public TiltingHandler tiltingHandler = new TiltingHandler(7);
 
     public EntityPassengerDuewagGT6ZRTail(World world) {
